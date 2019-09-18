f57e993b7edbe5661c7337a6569144b60fe21b0c
Katie Cruise <katiecruise23@gmail.com>
2019-09-17 20:55:12 -0600

    completed node-4-afternoon

---
+++ server/index.js
@@ -0,0 +1,35 @@
+require('dotenv').config()
+const express = require('express')
+const session = require('express-session')
+const app = express()
+const {SERVER_PORT, SESSION_SECRET} = process.env
+const checkForSession = require('../server/middlewares/checkForSession')
+const swagCtrl = require('./controllers/swagController')
+const authCtrl = require('./controllers/authController')
+const cartCtrl = require('./controllers/cartController')
+const searchCtrl = require('./controllers/searchController')
+
+app.use(express.static(`${__dirname}/../build`))
+app.use(express.json())
+app.use(session({
+    resave: false,
+    saveUninitialized: false,
+    secret: SESSION_SECRET
+}))
+app.use(checkForSession)
+
+
+app.get('/api/swag', swagCtrl.read)
+
+app.post('/api/login', authCtrl.login)
+app.post('/api/register', authCtrl.register)
+app.post('/api/signout', authCtrl.signout)
+app.get('/api/user', authCtrl.getUser)
+
+app.post('/api/cart/checkout', cartCtrl.checkout)
+app.post('/api/cart/:id', cartCtrl.add)
+app.delete('/api/cart/:id', cartCtrl.delete)
+
+app.get('/api/search', searchCtrl.search)
+
+app.listen(SERVER_PORT, () => console.log(`${SERVER_PORT} chill peeps`))
